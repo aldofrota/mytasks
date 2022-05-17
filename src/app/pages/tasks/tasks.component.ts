fd7f75a269e2fcbbc38fa8ae9511a3da6870f3aa
Aldo Frota <aldofrotadev@gmail.com>
2022-05-17 13:33:17 -0300

    finalizada a tela de taks com registro, edição e exclusão de tasks

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,159 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+type tTask = {
+  id: number;
+  title: string;
+  description: string;
+  openAt: string;
+};
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks = [
-    {
-      id: 1,
-      title: 'Task 1',
-      description: 'Task description 1',
-      time: '17:51',
-    },
-    {
-      id: 2,
-      title: 'Task 2',
-      description: 'Task description 2',
-      time: '17:52',
-    },
-    {
-      id: 3,
-      title: 'Task 3',
-      description: 'Task description 3',
-      time: '17:53',
-    },
-    {
-      id: 4,
-      title: 'Task 4',
-      description: 'Task description 4',
-      time: '17:54',
-    },
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 6,
-      title: 'Task 6',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 7,
-      title: 'Task 7',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 8,
-      title: 'Task 8',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 9,
-      title: 'Task 9',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 10,
-      title: 'Task 10',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 11,
-      title: 'Task 11',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 12',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 13',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 14',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 15',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 16',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 17',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Task description 5',
-      time: '17:55',
-    },
-  ];
+  tasks: Array<tTask> = new Array();
+
+  ids: number = 0;
 
   constructor() {}
+
+  doubleClick(event: any, id: number): void {
+    event.target.disabled = !event.target.disabled;
+    event.target.focus();
+    event.path[2].style.borderColor = '#70e858';
+  }
+
+  onBlur(event: any, id: number): void {
+    event.target.disabled = true;
+    event.path[2].style.borderColor = '#e5e7eb';
+  }
+
+  addTask(): void {
+    this.ids = this.ids + 1;
+    this.tasks.push({
+      id: this.ids,
+      title: '',
+      description: '',
+      openAt: new Date().getHours() + ':' + new Date().getMinutes() + 'h',
+    });
+    console.log('Task added' + this.ids);
+  }
+
+  deleteTask(id: number): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 
   ngOnInit(): void {}
 }
